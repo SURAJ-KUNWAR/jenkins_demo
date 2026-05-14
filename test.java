523b1b87e85e075a6377ed84445fdb1a5de4237f
suraj <kunwarsuraj19@gmail.com>
2026-05-14 14:00:59 +0530

    only add kept

--- test.java
+++ test.java
@@ -13,8 +13,8 @@ public class test {
    public static void main(String[] args) {
        test t = new test();
       //  t.add(1, 2);
-      //  t.sub(10, 5);
-      //  t.mul(10,5);
+        t.sub(10, 5);
+        t.mul(10,5);
 
    }
 }
