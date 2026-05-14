6aba297fb7fafd7ce31aa138922f1b60458a7cdc
suraj <kunwarsuraj19@gmail.com>
2026-05-14 13:03:24 +0530

    javafile added

---
+++ test.java
@@ -0,0 +1,17 @@
+public class test {
+   public void add (int a , int b){
+     System.out.println("addition" +"->" +   a+b);
+   } 
+
+     public void sub (int a , int b){
+     System.out.println("subtraction" +"->" +   (a-b));
+   } 
+
+     public void mul (int a , int b){
+     System.out.println("multiplication" +"->" +   a*b);
+   } 
+   public static void main(String[] args) {
+       test t = new test();
+       t.add(1, 2);
+   }
+}
